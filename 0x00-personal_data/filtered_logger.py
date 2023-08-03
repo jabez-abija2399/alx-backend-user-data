23cea3ca7db1fe17ce32ed256ebfa4fce46493a5
Ebenezerkifle <111049968+jabez-abija2399@users.noreply.github.com>
2023-08-03 01:51:16 -0700

    the format method to filter values in incoming log records using filter_datum. Values for fields in fields should be filtered.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 """ Regex-ing, Log formatter, Create logger, Connect to secure database,
     Read and filter data """
+from typing import List
 import re
+import logging
+import os
+import mysql.connector
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 """ containing the fields from user_data.csv that are considered PII. """
 
@@ -13,6 +17,19 @@ class RedactingFormatter(logging.Formatter):
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ constructor """
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ filter values in incoming log records """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+
 
 if __name__ == "__main__":
     main()
